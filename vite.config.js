f6de36fa557f6cad20a4f19967e7dcf43ac49966
sally mohammed <sallymohammed1521999@gmail.com>
2026-08-09 21:49:18 +0300

    Fix index.html script source and favicon paths

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/brightedge-technologies/',
   plugins: [
     vue(),
     vueDevTools(),
